fc4044c27e36133e7e843e4d86e144db5a3cdfd5
Atharv Goel <agoel@deqode.com>
2024-10-16 00:02:57 +0530

    feat: add test case for different delimeters

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,9 +1,15 @@
 export class StringCalculator {
   add(numbers: string): number {
-    if (numbers === "") {
-      return 0;
+    if (numbers === '') return 0;
+
+    let delimiter = /,|\n/;
+    if (numbers.startsWith('//')) {
+      const delimiterEnd = numbers.indexOf('\n');
+      delimiter = new RegExp(numbers.slice(2, delimiterEnd));
+      numbers = numbers.slice(delimiterEnd + 1);
     }
-    const numberArray = numbers.split(/,|\n/).map(Number);
+
+    const numberArray = numbers.split(delimiter).map(Number);
     return numberArray.reduce((sum, num) => sum + num, 0);
   }
 }
--- tests/string-calculator.test.ts
+++ tests/string-calculator.test.ts
@@ -26,4 +26,8 @@ describe("String Calculator", () => {
   test('should handle new lines between numbers', () => {
     expect(calculator.add('1\n2,3')).toBe(6);
   });
+
+  test('should support different delimiters', () => {
+    expect(calculator.add('//;\n1;2')).toBe(3);
+  });
 });
